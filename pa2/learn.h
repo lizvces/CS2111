a03e7cf9839fe2abeff17f506f65b575e6cefe20
lizvcespedes <lizvcespedes@gmail.com>
2018-02-23 10:36:58 -0500

    Add files via upload

---
+++ pa2/learn.h
@@ -0,0 +1,8 @@
+#ifndef LEARN_H_
+#define LEARN_H_
+
+double** transpose(double**, int, int);
+double** multiply(double**, double**, int, int, int );
+double** reducedRow(double**, int, int, int);
+
+#endif
