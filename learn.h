8b75d546dbd96be5262dc4c59d7ce17d060762cc
lizvcespedes <lizvcespedes@gmail.com>
2018-02-23 10:37:24 -0500

    Delete learn.h

--- learn.h
+++
@@ -1,8 +0,0 @@
-#ifndef LEARN_H_
-#define LEARN_H_
-
-double** transpose(double**, int, int);
-double** multiply(double**, double**, int, int, int );
-double** reducedRow(double**, int, int, int);
-
-#endif
